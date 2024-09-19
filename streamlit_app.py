06ddd8793e05738f4c7027015cec4e7847a02b56
elhamod <55500282+elhamod@users.noreply.github.com>
2024-09-18 20:32:12 -0400

    add textbox

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,6 @@ st.title("I am so beautiful")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+title = st.text_input("Movie title", "Life of Brian")
+st.write("The current movie title is", title)
